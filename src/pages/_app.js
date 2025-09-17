2313df5bc87fbc1f9c850df2f130a3d7725206c4
michaelrose01795 <michaelrose01795@gmail.com>
2025-09-17 17:32:54 +0000

    push

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,25 @@
 // src/pages/_app.js
-import '../styles/global.css'  // relative path from _app.js to global.css
+import { useEffect, useState } from "react";
+import keycloak from "../auth/Keycloak";
+import "../styles/global.css";  // relative path from _app.js to global.css
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    keycloak
+      .init({ onLoad: "login-required", checkLoginIframe: false })
+      .then(authenticated => {
+        setIsAuthenticated(authenticated);
+      })
+      .catch(err => {
+        console.error("Keycloak init error:", err);
+      });
+  }, []);
+
+  if (!isAuthenticated) {
+    return <div>Loading authentication...</div>;
+  }
+
+  return <Component {...pageProps} />;
 }
