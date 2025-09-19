4bcbebf5cf0534e4960765709e421efc58e88d1d
michaelrose01795 <michaelrose01795@gmail.com>
2025-09-19 18:41:38 +0000

    g

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,14 +4,14 @@ import Sidebar from "./Sidebar";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex flex-row min-h-screen"> 
       {/* Sidebar on the left */}
       <Sidebar />
 
       {/* Main content area */}
-      <main className="flex-1 bg-gray-100 p-6 overflow-y-auto">
+      <div className="flex-1 bg-gray-100 p-6 overflow-y-auto">
         {children}
-      </main>
+      </div>
     </div>
   );
 }
