7cf4926ba6615e9f01b43c485cb7d3a8f77f851e
michaelrose01795 <michaelrose01795@gmail.com>
2025-09-19 11:34:17 +0000

    g

--- src/app/login/topbar/TopBar.tsx
+++ src/app/login/topbar/TopBar.tsx
@@ -2,7 +2,7 @@
 "use client"; // must be first line to allow React hooks and client-side code
 
 import React from "react"; // import React
-import { useAuth } from "../context/UserContext"; // import authentication context for user info & logout
+import { useAuth } from "../../../context/UserContext"; // import authentication context for user info & logout
 import "./TopBar.css"; // import styling for the topbar
 
 const TopBar = () => {
